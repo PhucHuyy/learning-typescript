604619927fe48c483e15b5ff2cff3cebde9ca6c5
Nguyễn Huy Phúc <huyphuc280503@gmail.com>
2025-03-24 18:42:54 +0700

    Refactor App component to use CRUDStudentPage and PrimeReactProvider; update styles for button component

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,20 @@
-import React from "react";
-import "./App.css";
-import ShowListStudent from "./ShowListStudent";
+import { ToastContainer } from 'react-toastify';
+import './App.css';
+import CRUDStudentPage from './page/CRUDStudentPage';
+
+import { PrimeReactProvider } from 'primereact/api';
+import 'primereact/resources/themes/lara-light-cyan/theme.css';
 
 function App() {
   return (
-    <div>
-      {/* <Test /> */}
-      <ShowListStudent />
-    </div>
+    <>
+      <PrimeReactProvider>
+        {/* <Test /> */}
+        {/* <ShowListStudent /> */}
+        <CRUDStudentPage />
+        <ToastContainer />
+      </PrimeReactProvider>
+    </>
   );
 }
 
